326fa3e0d0b5779c75e723244cf14a462f09dce5
Mekkit <hmek1987@gmail.com>
2024-09-15 00:33:06 +0700

    nonsleep_GIN

--- workflow-api/cmd/workflow-api/main.go
+++ workflow-api/cmd/workflow-api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//3 2 1 Let Go
+	// fmt.Println("Hello, World!")
+	//Sip the GIN
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	// Start server - short hand error handling
+	if err := r.Run(); err != nil {
+		log.Panic(err)
+	}
+
 }
