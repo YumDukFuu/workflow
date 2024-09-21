106de464599975191ccff42b03509a41797ea79d
Mekkit <hmek1987@gmail.com>
2024-09-21 10:02:27 +0700

    Nonsleep_ENV_Port

--- workflow-api/cmd/workflow-api/main.go
+++ workflow-api/cmd/workflow-api/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/YumDukFuu/workflow/internal/item"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,6 +18,15 @@ import (
 // PATCH    /items/:id
 // DELETE	/items/:id
 func main() {
+	// Test Env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Print Foo:Bar
+	// fmt.Println("FOO:", os.Getenv("FOO"))
+	// fmt.Println("Test:", os.Getenv("TEST"))
+
 	// Connect database
 	db, err := gorm.Open(
 		postgres.Open(
@@ -33,18 +44,34 @@ func main() {
 	// Router
 	r := gin.Default()
 
-	// Register 📝 router
-	r.POST("/items", controller.CreateItem)
-	// Register 🔎 router
-	r.GET("/items", controller.FindItems)
-	// Register ✍️ router
-	r.PATCH("/items/:id", controller.UpdateItemStatus)
-	// Register 📂 router /items/:id?title=ขอเบิกสินค้า&amount=12&quantity=10
-	r.GET("/items/:id", controller.FindEachItem)
-	// Register ✏️ router
-	r.PUT("/items/:id", controller.EditItem)
-	// Register 🗑️ router
-	r.DELETE("/items/:id", controller.DeleteItem)
+	item := r.Group("/items")
+	{
+		// Register 📝 router
+		item.POST("", controller.CreateItem)
+		// Register 🔎 router
+		item.GET("", controller.FindItems)
+		// Register ✍️ router
+		item.PATCH("/:id", controller.UpdateItemStatus)
+		// Register 📂 router /items/:id?title=ขอเบิกสินค้า&amount=12&quantity=10
+		item.GET("/:id", controller.FindEachItem)
+		// Register ✏️ router
+		item.PUT("/:id", controller.EditItem)
+		// Register 🗑️ router
+		item.DELETE("/:id", controller.DeleteItem)
+	}
+
+	// // Register 📝 router
+	// r.POST("/items", controller.CreateItem)
+	// // Register 🔎 router
+	// r.GET("/items", controller.FindItems)
+	// // Register ✍️ router
+	// r.PATCH("/items/:id", controller.UpdateItemStatus)
+	// // Register 📂 router /items/:id?title=ขอเบิกสินค้า&amount=12&quantity=10
+	// r.GET("/items/:id", controller.FindEachItem)
+	// // Register ✏️ router
+	// r.PUT("/items/:id", controller.EditItem)
+	// // Register 🗑️ router
+	// r.DELETE("/items/:id", controller.DeleteItem)
 
 	///🚧///🚧///🚧///🚧///🚧///🚧///🚧///🚧///🚧///🚧///
 	// r.DELETE("/items/:id", func(ctx *gin.Context) {
@@ -58,8 +85,14 @@ func main() {
 	// })
 
 	// Start server
-	if err := r.Run(); err != nil {
-		log.Panic(err)
+	// if err := r.Run(); err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// ENV Port
+	port := os.Getenv("PORT")
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal(err)
 	}
 
 	// //3 2 1 Let Go
